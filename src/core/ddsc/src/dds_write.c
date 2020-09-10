c8cea14334f08db42dd424275e27bba333954c11
Joshua Whitley <josh.whitley@autowarefoundation.org>
2020-09-09 19:11:35 -0500

    Rebase from 'release/eloquent/cyclonedds'

--- src/core/ddsc/src/dds_write.c
+++ src/core/ddsc/src/dds_write.c
@@ -90,43 +90,27 @@ struct local_sourceinfo {
   const struct ddsi_sertopic *src_topic;
   struct ddsi_serdata *src_payload;
   struct ddsi_tkmap_instance *src_tk;
-  nn_mtime_t timeout;
+  ddsrt_mtime_t timeout;
 };
 
 static struct ddsi_serdata *local_make_sample (struct ddsi_tkmap_instance **tk, struct ddsi_domaingv *gv, struct ddsi_sertopic const * const topic, void *vsourceinfo)
 {
   struct local_sourceinfo *si = vsourceinfo;
-  if (topic == si->src_topic)
+  struct ddsi_serdata *d = ddsi_serdata_ref_as_topic (topic, si->src_payload);
+  if (d == NULL)
   {
-    *tk = si->src_tk;
-    /* FIXME: see if this pair of refc increments can't be avoided
-       They're needed because free_sample_after_delivery will always be called, but
-       in the common case of a local writer and a single sertopic, make_sample doesn't
-       actually create a sample, and so free_sample_after_delivery doesn't actually
-       have to free anything */
-    ddsi_tkmap_instance_ref (si->src_tk);
-    return ddsi_serdata_ref (si->src_payload);
+    DDS_CWARNING (&gv->logconfig, "local: deserialization %s/%s failed in topic type conversion\n", topic->name, topic->type_name);
+    return NULL;
   }
+  if (topic != si->src_topic)
+    *tk = ddsi_tkmap_lookup_instance_ref (gv->m_tkmap, d);
   else
   {
-    /* ouch ... convert a serdata from one sertopic to another ... */
-    ddsrt_iovec_t iov;
-    uint32_t size = ddsi_serdata_size (si->src_payload);
-    (void) ddsi_serdata_to_ser_ref (si->src_payload, 0, size, &iov);
-    struct ddsi_serdata *d = ddsi_serdata_from_ser_iov (topic, si->src_payload->kind, 1, &iov, size);
-    ddsi_serdata_to_ser_unref (si->src_payload, &iov);
-    if (d)
-    {
-      d->statusinfo = si->src_payload->statusinfo;
-      d->timestamp = si->src_payload->timestamp;
-      *tk = ddsi_tkmap_lookup_instance_ref (gv->m_tkmap, d);
-    }
-    else
-    {
-      DDS_CWARNING (&gv->logconfig, "local: deserialization %s/%s failed in topic type conversion\n", topic->name, topic->type_name);
-    }
-    return d;
+    // if the topic is the same, we can avoid the lookup
+    ddsi_tkmap_instance_ref (si->src_tk);
+    *tk = si->src_tk;
   }
+  return d;
 }
 
 static dds_return_t local_on_delivery_failure_fastpath (struct entity_common *source_entity, bool source_entity_locked, struct local_reader_ary *fastpath_rdary, void *vsourceinfo)
@@ -136,9 +120,9 @@ static dds_return_t local_on_delivery_failure_fastpath (struct entity_common *so
   assert (source_entity->kind == EK_WRITER);
   struct writer *wr = (struct writer *) source_entity;
   struct local_sourceinfo *si = vsourceinfo;
-  nn_mtime_t tnow = now_mt ();
+  ddsrt_mtime_t tnow = ddsrt_time_monotonic ();
   if (si->timeout.v == 0)
-    si->timeout = add_duration_to_mtime (tnow, wr->xqos->reliability.max_blocking_time);
+    si->timeout = ddsrt_mtime_add_duration (tnow, wr->xqos->reliability.max_blocking_time);
   if (tnow.v >= si->timeout.v)
     return DDS_RETCODE_TIMEOUT;
   else
@@ -267,8 +251,7 @@ void dds_write_flush (dds_entity_t writer)
 {
   struct thread_state1 * const ts1 = lookup_thread_state ();
   dds_writer *wr;
-  dds_return_t rc;
-  if ((rc = dds_writer_lock (writer, &wr)) == DDS_RETCODE_OK)
+  if (dds_writer_lock (writer, &wr) == DDS_RETCODE_OK)
   {
     thread_state_awake (ts1, &wr->m_entity.m_domain->gv);
     nn_xpack_send (wr->m_xp, true);
